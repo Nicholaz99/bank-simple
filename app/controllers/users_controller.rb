3d4e6ca4f8acc980aa75d775466d9b2e9417ef25
Nicholas <nicholasmagbanua@gmail.com>
2017-12-29 13:43:21 +0700

    Add validation and complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   
+  before_action :require_user, except: [:new, :index, :create]
   before_action :require_same_user, except: [:new, :create, :index]
 
   def index
